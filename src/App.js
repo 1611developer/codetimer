88d2e76fb1ce165fd03ea9406dc6f145a8e82e75
Philip M <101215986+1611developer@users.noreply.github.com>
2023-03-14 20:17:01 -0700

    got started with react components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import MoodItem from './components/MoodItem';
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <MoodItem></MoodItem>
     </div>
   );
 }
---
+++ src/components/MoodItem.js
@@ -0,0 +1,5 @@
+function MoodItem() {
+    return <h2>Mood Item!</h2>
+}
+
+export default MoodItem; 
